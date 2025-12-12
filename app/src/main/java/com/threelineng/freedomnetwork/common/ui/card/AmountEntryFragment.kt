8afa6ce7bcd02f4095247c41a2e0622e8951e510
Oluwatosin Ayoola <Oluwatosin.Ayoola@xpresspayments.com>
2025-12-12 23:49:17 +0100

    Added amount entry and insert card screen

--- app/src/main/java/com/threelineng/freedomnetwork/common/ui/card/AmountEntryFragment.kt
+++ app/src/main/java/com/threelineng/freedomnetwork/common/ui/card/AmountEntryFragment.kt
@@ -1,14 +1,20 @@
 package com.threelineng.freedomnetwork.common.ui.card
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.tegritech.commons.utils.viewBinding
 import com.threelineng.freedomnetwork.R
+import com.threelineng.freedomnetwork.databinding.FragmentAmountEntryBinding
 
 class AmountEntryFragment : Fragment(R.layout.fragment_amount_entry) {
+    private val binding by viewBinding<FragmentAmountEntryBinding>()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.continueButton?.setOnClickListener {
+            findNavController().navigate(R.id.action_amountEntryFragment_to_insertCardFragment)
+        }
     }
 }

fbc119864daa09f5ab446f3aca670914e6bea6f2
Oluwatosin Ayoola <Oluwatosin.Ayoola@xpresspayments.com>
2025-12-14 17:17:04 +0100

    added account selection screens

---
+++ app/src/main/java/com/threelineng/freedomnetwork/withdrawal/SelectAccountTypeFragment.kt
@@ -0,0 +1,16 @@
+package com.threelineng.freedomnetwork.withdrawal
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.threelineng.freedomnetwork.R
+
+class SelectAccountTypeFragment : BottomSheetDialogFragment(R.layout.fragment_select_account_type) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
